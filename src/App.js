369f847cd01bbef0ebf77582f3c67bcf9dabac63
Will Quanstrom <wquanstr215@gmail.com>
2020-02-21 08:26:14 -0800

    fix react-router bug

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import Footer from './components/Footer';
 import Splash from './components/Splash';
 import Customers from './components/Customers';
 import Employees from './components/Employees';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <NavBar />
-      <BrowserRouter>
-        <div>
-          <Route exact path='/' component={Splash} />
-          <Route path='/customers' component={Customers} />
-          <Route path='/employees' component={Employees} />
-        </div>
-      </BrowserRouter>
-      <Footer />
-    </div>
+    <HashRouter>
+      <div>
+        <Header />
+        <NavBar />
+          <div>
+            <Route exact path='/' component={Splash} />
+            <Route path='/customers' component={Customers} />
+            <Route path='/employees' component={Employees} />
+          </div>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
